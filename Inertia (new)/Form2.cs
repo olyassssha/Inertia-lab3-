85768ca857881f9ee0279f10c2382b84e7be54a4
olyassssha <73666793+olyassssha@users.noreply.github.com>
2021-06-14 21:25:22 +0300

    removed the inheritance from lckass Logic to class Element

--- Inertia (new)/Form2.cs
+++ Inertia (new)/Form2.cs
@@ -19,8 +19,18 @@ namespace Inertia__new_
         Label label2 = new Label();
         private void Instruction_Click(object sender, EventArgs e)
         {
-            this.Close();
-           Application.OpenForms[0].Show();
+        
+            button1.BackColor = Color.Red;
+       
+           
+            label.Location = new Point(button1.Location.X+80, button1.Location.Y+60);
+            label.AutoSize = true;
+            label.Text = "Операционная система: Windows 7, 32 бит \n" +
+                         "Оперативная память: 130 Кб \n" +
+                         "Необходимое место на диске: 6 Мб";
+            label.Font = new Font("Calibri", 12);
+            label.Show();
+            Controls.Add(label);
         }
 
         private void button2_Click(object sender, EventArgs e)
